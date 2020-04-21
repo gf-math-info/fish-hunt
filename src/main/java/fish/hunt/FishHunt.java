ba43141f9f15d1ab47a22422ddbccab73271c7af
GFL0123456789 <gabriel.fortin-leblanc@umontreal.ca>
2020-04-21 11:25:36 -0400

    Import de Javafx

---
+++ src/main/java/fish/hunt/FishHunt.java
@@ -0,0 +1,16 @@
+package fish.hunt;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FishHunt extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //TODO : FishHunt.start(Stage) à implémenter.
+    }
+}
