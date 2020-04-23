e69ae8e3e7caa775635b9fcff9f8802be9b3f597
GFL0123456789 <gabriel.fortin-leblanc@umontreal.ca>
2020-04-22 22:39:37 -0400

    Création de PartieEtat

---
+++ src/main/java/fish/hunt/modele/PartieEtat.java
@@ -0,0 +1,14 @@
+package fish.hunt.modele;
+
+/**
+ * Une énumération des différents états d'une partie.
+ * @author Fortin-Leblanc, Gabriel
+ * @author Colson-Ratelle, Antoine
+ */
+public enum PartieEtat {
+
+    EN_COURS,
+    EN_PAUSE,
+    PERDU
+
+}
