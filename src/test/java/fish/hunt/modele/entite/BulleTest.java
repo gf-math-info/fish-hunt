6edde749d1a6ad1ef2613dbee13d035035f49a3c
GFL0123456789 <gabriel.fortin-leblanc@umontreal.ca>
2020-04-24 21:06:05 -0400

    Tests complétés

--- src/test/java/fish/hunt/modele/entite/BulleTest.java
+++ src/test/java/fish/hunt/modele/entite/BulleTest.java
@@ -2,9 +2,37 @@ package fish.hunt.modele.entite;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BulleTest {
 
     @Test
     public void testActualiser() {
+        double deltaTemps = 1e-5;
+        double accepte = 0.001;
+
+        Bulle bulle = new Bulle(50, 0, 0, 0);
+
+        for(int i = 0; i < 1e5; i++)
+            bulle.actualiser(deltaTemps);
+
+        assertEquals(0, bulle.getX(), accepte);
+        assertEquals(0, bulle.getY(), accepte);
+
+        bulle = new Bulle(50, 0, 0, 100);
+
+        for(int i = 0; i < 1e5; i++)
+            bulle.actualiser(deltaTemps);
+
+        assertEquals(0, bulle.getX(), accepte);
+        assertEquals(100, bulle.getY(), accepte);
+
+        bulle = new Bulle(50, 0,0, -250);
+
+        for(int i = 0; i < 1e5; i++)
+            bulle.actualiser(deltaTemps);
+
+        assertEquals(0, bulle.getX(), accepte);
+        assertEquals(-250, bulle.getY(), accepte);
     }
 }
--- src/test/java/fish/hunt/modele/entite/ProjectileTest.java
+++ src/test/java/fish/hunt/modele/entite/ProjectileTest.java
@@ -1,16 +1,55 @@
 package fish.hunt.modele.entite;
 
+import fish.hunt.modele.entite.poisson.Poisson;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ProjectileTest {
 
+    private double deltaTemps = 1e-5;
+    private double accepte = 0.001;
+    private double tempsArrive = 1e5 / 6;
+
     @Test
     public void testIntersect() {
+
+        Poisson poisson = new Poisson(100, 0, 0, 0, 0);
+
+        Projectile projectile = new Projectile(0, 0);
+        assertTrue(projectile.intersect(poisson));
+
+        projectile = new Projectile(-1, 0);
+        assertFalse(projectile.intersect(poisson));
+
+        projectile = new Projectile(50, 50);
+        assertTrue(projectile.intersect(poisson));
+
+        projectile = new Projectile(101, 101);
+        assertFalse(projectile.intersect(poisson));
+
+        projectile = new Projectile(50, 101);
+        assertFalse(projectile.intersect(poisson));
     }
 
     @Test
     public void testActualiser() {
+        Projectile projectile = new Projectile(0, 0);
+        for(int i = 0; i < tempsArrive; i++)
+            projectile.actualiser(deltaTemps);
+
+        assertEquals(0, projectile.getDiametre(), accepte);
+
+        projectile = new Projectile(0, 0);
+        for(int i = 0; i < tempsArrive + 1e4; i++)
+            projectile.actualiser(deltaTemps);
+
+        assertEquals(0, projectile.getDiametre(), accepte);
+
+        projectile = new Projectile(0, 0);
+        for(int i = 0; i < 20; i++)
+            projectile.actualiser(deltaTemps);
+
+        assertEquals(49.94, projectile.getDiametre(), accepte);
     }
 }
